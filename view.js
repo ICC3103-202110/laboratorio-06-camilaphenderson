a6a1c6055c798024f263fcf3ecbb061d2ed1744e
camilaphenderson <cvpellegrini@miuandes.cl>
2021-05-24 08:57:54 -0400

    finished view

--- view.js
+++ view.js
@@ -16,7 +16,7 @@ function createTitle(){
     )
 }
 
-function getTable(model){
+function createTable(model){
     return [
         {
         'Left Value': model.leftValue,
@@ -25,4 +25,53 @@ function getTable(model){
         'Right Unit': model.rightUnit
         }
     ]
+}
+
+function input(model){
+    return inquirer.prompt([
+        {
+            name: 'YesNo',
+            type: 'list',
+            message: 'Left temperature is source?',
+            choices: ['Yes','No'],
+            default: 'N/A'
+        },
+        { 
+        type: 'input',
+        name: 'value',
+        message: 'Temperature value to convert?',
+        default: 'N',
+        validate: function(value){
+            if(isNaN(value) === true ){
+                return `Please enter a valid numeber`
+            } else {
+                return true}
+            }
+        },
+        {
+        type: 'list',
+        name:'from',
+        message: 'From?',
+        choices: ['Celsius','Fahrenheit','Kelvin'],
+        default: 'N'},
+        {
+        type: 'list',
+        name:'to',
+        message: 'To?',
+        choices: ['Celsius','Fahrenheit','Kelvin'],
+        default: rightUnit}
+    ])
+}
+
+function view(model){
+    return {
+        title: createTitle(),
+        table: createTable(model)
+    }
+}
+
+module.exports = {
+    view,
+    input,
+
 }
