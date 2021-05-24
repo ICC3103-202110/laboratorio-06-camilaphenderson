09813c6ee4698c5fff8555c1d9e6e6f540f920c8
camilaphenderson <cvpellegrini@miuandes.cl>
2021-05-24 10:18:22 -0400

    added temp converter

--- update.js
+++ update.js
@@ -15,6 +15,25 @@ function update(input,model){
             rightValue: input.value,
             rightUnit: input.to
         }
-
+    
+function converter(input,model){
+    if (input1.lunit == 'Celsius' && input1.runit =='Fahrenheit'){
+        return newValue = ((input.value)*1.8)+32
+        }
+    else if (input1.lunit == 'Celsius' && input1.runit =='Kelvin'){
+        return  newValue = (input.value) + 273.15
+    }
+    else if (input1.lunit == 'Fahrenheit' && input1.runit =='Kelvin'){
+        return newValue = (((input.value)-32)*5/9) + 273.15 
+    }
+    else if (input1.lunit == 'Fahrenheit' && input1.runit =='Celsius'){
+        return newValue =  ((input.value)-32)*5/9
+    }
+    else if (input1.lunit == 'Kelvin' && input1.runit =='Celsius'){
+        return newValue = (input.value) -273.15
+    }
+    else if (input1.lunit == 'Kelvin' && input1.runit =='Fahrenheit'){
+        return newValue = (((input.value)- 273.15)* 1.8) + 32
+    }
     
 }
